61fc5d1cbd7808e20d01312df29b86f8d81364f0
Shramee Srivastav <shramee.srivastav@gmail.com>
2021-09-07 16:29:22 +0800

    Tachyons starter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="tc bg-black-80 h-100 flex flex-column items-center justify-center f4 white">
+			<img src={logo} className="w-30 mb4" alt="logo"/>
+			<p>
+				Edit <code>src/App.js</code> and save to reload.
+			</p>
+			<a className="App-link" href="https://github.com/leastimperfect/react-starter" target="_blank">
+				GitHub: <code>leastimperfect/react-starter</code>
+			</a>
+		</div>
+	);
 }
 
 export default App;
